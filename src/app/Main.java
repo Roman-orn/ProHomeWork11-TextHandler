3abce45383ce8325c9046f97a7fae667b6448312
Roman-orn <0671635566@ukr.net>
2026-05-09 14:08:40 +0300

    Added functionality Main class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,26 @@
 package app;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
+
+    private static final String BASE_PATH = "files/";
+
+    static void main(String[] args) {
+
+        String fileName = "myFile";
+        String content = "Super information.";
+        String path = BASE_PATH + fileName + ".txt";
+
+        FileHandler handler = new FileHandler();
+
+        getOutput(handler.createFile(path));
+        getOutput(handler.writeToFile(Paths.get(path), content));
+        getOutput("CONTENT: " + handler.readFromFile(path));
+    }
+
+    static void getOutput(String output) {
+        System.out.println(output);
+    }
 }
